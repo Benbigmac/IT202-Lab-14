169fd7fd0a2fbad771f351853cb7d507563073c7
Bennett Maciorowski <bmacio2@uic.edu>
2018-11-26 23:33:26 +0000

    working on js buttons

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function onSignIn(googleUser) {
     var y =  document.getElementById("logOut");
     if (x.style.display === "none") {
         x.style.display = "block";
-        y.style.display = "none";
+      //  y.style.display = "none";
     } else {
         x.style.display = "none";
-        y.style.display = "block";
+        y.style.display = "inline";
     }
     
 }
