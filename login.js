ce4fb9585d960755afc709e1843b83e66e0e0d4f
Bennett Maciorowski <bmacio2@uic.edu>
2018-11-26 23:39:23 +0000

    more js fixes for ease of access

---
+++ login.js
@@ -0,0 +1,30 @@
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    alert(profile.getName());
+    var x = document.getElementById("sign");
+    var y =  document.getElementById("logOut");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+      //  y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "inline";
+        window.reload();
+    }
+    
+}
+
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+        alert("You've logged Out");
+        window.reload();
+    });
+  }
+
